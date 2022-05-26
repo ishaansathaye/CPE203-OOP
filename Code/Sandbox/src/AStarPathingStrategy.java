1cf97ddf8252b90f411cf40524c911bced7a92b6
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-05-25 19:32:32 -0700

    added comments to a *

--- Code/Sandbox/src/AStarPathingStrategy.java
+++ Code/Sandbox/src/AStarPathingStrategy.java
@@ -33,6 +33,7 @@ class AStarPathingStrategy implements PathingStrategy {
         }
     }
 
+    // Manhattan distance better than euclidean distance for this circumstance
     private static int manhattanDistance(Point p1, Point p2) {
         return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
     }
@@ -50,18 +51,21 @@ class AStarPathingStrategy implements PathingStrategy {
         // Closed List
         HashMap <Point, Node> closedList = new HashMap<Point, Node>();
 
-        // Current node
+        // Current node - start node
         Node currentNode = new Node(start, null, 0, 0, 0);
 
+        // Adding start node to the open list
         openList.add(currentNode);
         openListMap.put(start, currentNode);
 
         while (!openList.isEmpty()) {
+            // Retrieve the node with the lowest f value and mark it as the current node
             currentNode = openList.remove();
             openListMap.remove(currentNode.position);
 
-
+            // Checking if current node is adjacent to the end node
             if (withinReach.test(currentNode.position, end)) {
+                // Build path by traversing backwards from current node
                 while (currentNode.prevNode != null) {
                     path.add(0, currentNode.position);
                     currentNode = currentNode.prevNode;
@@ -69,13 +73,18 @@ class AStarPathingStrategy implements PathingStrategy {
                 return path;
             }
 
+            // Get the neighbors of the current node using apply from Function - template from SingleStepPathingStrategy
             List<Point> neighbors = potentialNeighbors.apply(currentNode.position).filter(canPassThrough).filter(pt -> !closedList.containsKey(pt)).collect(Collectors.toList());
 
+            // Analyze each neighbor
             for (Point neighborPt : neighbors) {
                 
+                // Calculate the g, h, and f values -> used heuristic (Manhattan distance)
                 Node neighborNode = new Node(neighborPt, currentNode, currentNode.g + 1, manhattanDistance(neighborPt, end), currentNode.g + 1 + manhattanDistance(neighborPt, end));
                 
+                // If the node is already on the open list
                 if (openListMap.containsKey(neighborPt)) {
+                    // If calculated g value is better than prev g value -> replace old with new
                     Node openListNode = openListMap.get(neighborPt);
                     if (openListNode.g > neighborNode.g) {
                         openList.remove(openListNode);
@@ -84,15 +93,18 @@ class AStarPathingStrategy implements PathingStrategy {
                         openListMap.put(neighborPt, neighborNode);
                     }
                 } else {
+                    // Not on the open list, so add it
                     openList.add(neighborNode);
                     openListMap.put(neighborPt, neighborNode);
                 }
             }
-
+            
+            // Moving current node to the closed List
             closedList.put(currentNode.position, currentNode);
 
         }
 
+        // Here if there is no path found
         return path;
     }
 }
